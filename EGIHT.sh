06ede2d78bafa56d8219cd9a3ff11ecb86c2bf80
Simisonka2022 <116421267+Simisonka2022@users.noreply.github.com>
2022-11-04 17:12:25 +0100

    Add files via upload

--- EGIHT.sh
+++ EGIHT.sh
@@ -13,18 +13,43 @@ echo "======================================"
 echo "Rendben beléphet"
 sleep 3
 clear
-
+spinner=( Ooooo oOooo ooOoo oooOo ooooO oooOo ooOoo oOooo);
+ 
+cat << EOF
+Loading
+EOF
+ 
+ 
+count(){
+  spin &
+  pid=$!
+ 
+  for i in `seq 1 10`
+  do
+    sleep 1;
+  done
+ 
+  kill $pid  
+}
+ 
+spin(){
+  while [ 1 ]
+  do 
+    for i in ${spinner[@]}; 
+    do 
+      echo -ne "\r$i";
+      sleep 0.2;
+    done;
+  done
+}
+ 
+count
 clear
 
-echo "  ______ _____ _____ _    _ _______ 
- |  ____/ ____|_   _| |  | |__   __|
- | |__ | |  __  | | | |__| |  | |   
- |  __|| | |_ | | | |  __  |  | |   
- | |___| |__| |_| |_| |  | |  | |   
- |______\_____|_____|_|  |_|  |_|   
-                                    
-                                    "
-
-sleep 1
-echo "Vége van $name"
-sleep 1
+echo -e "\e[31m  ______ _____ _____ _    _ _______ "
+ echo -e "\e[32m|  ____/ ____|_   _| |  | |__   __|"
+ echo -e "\e[33m| |__ | |  __  | | | |__| |  | |   "
+ echo -e "\e[34m|  __|| | |_ | | | |  __  |  | |   "
+ echo -e "\e[35m| |___| |__| |_| |_| |  | |  | | "  
+ echo -e "\e[36m|______\_____|_____|_|  |_|  |_|   "
+sleep 20
