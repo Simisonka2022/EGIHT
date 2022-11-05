ce84744fa43c7322f00700227691c64e5b5fa91e
Simisonka2022 <116421267+Simisonka2022@users.noreply.github.com>
2022-11-05 08:26:39 +0100

    Add files via upload

---
+++ EGIHT.py
@@ -0,0 +1,31 @@
+
+from pystyle import Colors, Colorate  
+import webbrowser
+import time
+from colorama import Fore
+print(Colorate.Horizontal(Colors.blue_to_cyan,"""
+                            ███████╗ ██████╗ ██╗██╗  ██╗████████╗
+                            ██╔════╝██╔════╝ ██║██║  ██║╚══██╔══╝
+                            █████╗  ██║  ███╗██║███████║   ██║   
+                            ██╔══╝  ██║   ██║██║██╔══██║   ██║   
+                            ███████╗╚██████╔╝██║██║  ██║   ██║   
+                            ╚══════╝ ╚═════╝ ╚═╝╚═╝  ╚═╝   ╚═╝""",1))
+
+time.sleep(2)
+print(Fore.RED + "                       ............................................")
+print(Fore.RED + "                   ....................Hacker Tools....................")
+print(Fore.RED + "                       ............................................")
+time.sleep(3)
+name = input(Fore.LIGHTWHITE_EX + ' Whats Your Name? ')
+print('.')
+time.sleep(1)
+print('.')
+time.sleep(1)
+print('.')
+time.sleep(2)
+print(' Welcome', name)
+time.sleep(3)
+print("""============================
+ [1] Hack Gmail Account                            
+============================""")
+time.sleep(1000000)
